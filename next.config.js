79dd15936cb8108d8628dc264239691f5113518e
aijeep815-eng <aijeep815@gmail.com>
2025-11-04 03:25:12 -0600

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   i18n: {
-    locales: ['en', 'zh', 'es'],
-    defaultLocale: 'en',
+    locales: ['en', 'zh', 'es'], // Supported languages
+    defaultLocale: 'en',         // Default to English
+    localeDetection: true,       // Automatically detect browser language
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'], // Allow AI images
   },
 };
+
 module.exports = nextConfig;
