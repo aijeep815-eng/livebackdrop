34729c5e52f26fb07e96dc1194e6bde6d4a13a7a
aijeep815-eng <aijeep815@gmail.com>
2025-11-04 03:39:42 -0600

    Add files via upload

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,13 +1,24 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import styles from '../styles/navbar.module.css';
 
 export default function LanguageSwitcher() {
   const router = useRouter();
   const { locale, pathname, asPath, query } = router;
 
+  useEffect(() => {
+    // Prefetch locale routes to avoid flicker on switch
+    ['en', 'zh', 'es'].forEach(lc => {
+      if (lc !== locale) {
+        router.prefetch({ pathname, query }, asPath, { locale: lc }).catch(() => {});
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [locale, pathname, asPath]);
+
   const changeLanguage = (newLocale) => {
     if (newLocale === locale) return;
-    router.push({ pathname, query }, asPath, { locale: newLocale });
+    router.push({ pathname, query }, asPath, { locale: newLocale, scroll: false }).catch(() => {});
   };
 
   return (
