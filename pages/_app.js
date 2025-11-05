d48b7e7f224ee7ae0e7cc2abc14b371b60d6d4b3
aijeep815-eng <aijeep815@gmail.com>
2025-11-05 02:56:18 -0600

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global.css';
 import '../styles/navbar.module.css';
+import '../styles/home.css';
 import NavBar from '../components/NavBar';
 
 export default function MyApp({ Component, pageProps }) {
