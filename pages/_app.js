9d9f08901800b35223b320c477e42c178108910a
aijeep815-eng <aijeep815@gmail.com>
2025-11-06 02:22:02 -0600

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import '../styles/layout.css';
-import '../styles/home.css';
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // 登录页和注册页使用完整布局
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
