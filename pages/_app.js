84c261c9c4b605f090dfbf0fcb05b1f6542b3713
aijeep815-eng <aijeep815@gmail.com>
2025-11-06 02:27:37 -0600

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  // 登录页和注册页使用完整布局
   return (
     <Layout>
       <Component {...pageProps} />
