4a7c747d8cea5a7811a45a2f7341afec6ad36159
aijeep815-eng <aijeep815@gmail.com>
2025-11-06 02:36:12 -0600

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,16 @@
-import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-white">
-      {/* 头部 */}
-      <Header />
+      {/* 顶部导航（包含 LOGO、菜单、多语言切换） */}
+      <NavBar />
 
       {/* 主体内容 */}
       <main className="flex-grow">{children}</main>
 
-      {/* 脚部 */}
+      {/* 底部版权 */}
       <Footer />
     </div>
   );
