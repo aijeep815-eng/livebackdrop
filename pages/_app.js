f28e33e2770fb4f64b930b60a9261f3d544d9672
aijeep815-eng <aijeep815@gmail.com>
2025-11-07 03:42:27 -0600

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <main style={{ minHeight: '70vh', paddingTop: '4rem' }}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  )
 }
