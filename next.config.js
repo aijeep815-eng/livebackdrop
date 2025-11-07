2a662117285839d757809efccce37a6a567a6e37
aijeep815-eng <aijeep815@gmail.com>
2025-11-07 03:10:57 -0600

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'zh', 'es'],
-    defaultLocale: 'en',
-    // Turn off browser-language redirects that cause full-page reloads.
-    localeDetection: false,
-  },
   reactStrictMode: true,
+  output: 'standalone', // force dynamic website build
+  experimental: {
+    esmExternals: false
+  }
 };
 
 module.exports = nextConfig;
