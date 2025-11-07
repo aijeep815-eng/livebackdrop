e4fb75572c0eb6b7f51b0fd0fb504c7dd24bea69
aijeep815-eng <aijeep815@gmail.com>
2025-11-07 02:26:52 -0600

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
-import dynamic from 'next/dynamic';
-import '../styles/globals.css';
-
-// Only use NavBar.js, remove any Nav.js reference
-const NavBar = dynamic(() => import('@/components/NavBar'), { ssr: false });
-const Footer = dynamic(() => import('@/components/Footer'), { ssr: false });
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <NavBar />
-      <main style={{ minHeight: '80vh', padding: '2rem 1rem', backgroundColor: '#f9fafb' }}>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </>
-  );
+  return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    const locale = this.props.__NEXT_DATA__?.locale || 'en';
-    return (
-      <Html lang={locale}>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
