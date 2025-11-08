54dfe47c06382a7ec9cd0af3e4c369b76f3af651
aijeep815-eng <aijeep815@gmail.com>
2025-11-08 01:41:58 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
-import '@/styles/globals.css'
-import NavBar from '@/components/NavBar'
-import Footer from '@/components/Footer'
+import '@/styles/globals.css'  // 确保 Tailwind 样式被加载
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <NavBar />
-      <main style={{ minHeight: '70vh', paddingTop: '4rem' }}>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </>
-  )
+  useEffect(() => {
+    // 在客户端确保 Tailwind 动态加载后刷新样式
+    document.body.classList.add('bg-gray-50')
+  }, [])
+
+  return <Component {...pageProps} />
 }
